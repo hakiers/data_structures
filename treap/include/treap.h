0e214c7ec93d6ee66adb6a90a2e1d3db1d8cdf5f
hakiers <mateusz.wojaczek@student.uj.edu.pl>
2026-01-31 12:58:27 +0100

    Fix bugs and add concepts

--- treap/include/treap.h
+++ treap/include/treap.h
@@ -3,47 +3,62 @@
 #include <memory>
 #include <random>
 #include <cstddef>
-
+#include <concepts>
 inline std::mt19937_64 rng{std::random_device{}()};
 
-template<typename T> 
-struct Node{
-    T value;
-    size_t priority;
-    std::unique_ptr<Node<T>> left;
-    std::unique_ptr<Node<T>> right;
+template <typename T>
+using Uptr = std::unique_ptr<T>;
 
-    Node(T value = {}) 
+template <typename T>
+concept Comparable = requires(const T& x, const T& y){
+    {x < y} -> std::convertible_to<bool>;
+    {x > y} -> std::convertible_to<bool>;
+    {x <= y} -> std::convertible_to<bool>;
+    {x >= y} -> std::convertible_to<bool>;
+    {x == y} -> std::convertible_to<bool>;
+};
+
+
+template<Comparable T> 
+class Treap{
+    struct Node{
+        T value;
+        size_t priority;
+        Uptr<Node> left;
+        Uptr<Node> right;
+
+        Node(T value = {}) 
         :
             value(value), 
             priority(rng()),
             left(nullptr),
             right(nullptr)
         {}
-};
+    };
 
-template<typename T> 
-std::unique_ptr<Node<T>> join(std::unique_ptr<Node<T>>, std::unique_ptr<Node<T>>);
+    Uptr<Node> root;
 
-template<typename T>
-std::pair<std::unique_ptr<Node<T>>, std::unique_ptr<Node<T>>> split(std::unique_ptr<Node<T>>, const T&);
+    static Uptr<Node> join(Uptr<Node>, Uptr<Node>);
 
-template<typename T> 
-class Treap{
-    private:
-        std::unique_ptr<Node<T>> root;
+    template<typename Compare>
+    static std::pair<Uptr<Node>, Uptr<Node>> split(Uptr<Node>, const T &);
+
+    static std::pair<Uptr<Node>, Uptr<Node>> splitL(Uptr<Node> x, const T& k) { return split<std::less_equal<T>>(std::move(x), k); }
+    static std::pair<Uptr<Node>, Uptr<Node>> splitU(Uptr<Node> x, const T& k) { return split<std::less<T>>(std::move(x), k); }
+
     public:
-        Treap() : root(nullptr) {};
+        Treap() = default;
+        ~Treap() = default;
         void insert(const T&);
         void erase(const T&);
-        bool contains(const T&);
+        bool contains(const T&) const;
         //todo
         //iterator 
         //lower/upper bound 
 };
 
-template<typename T>
-std::unique_ptr<Node<T>> join(std::unique_ptr<Node<T>> x, std::unique_ptr<Node<T>> y){
+template<Comparable T>
+Uptr<typename Treap<T>::Node> Treap<T>::join(Uptr<Node> x, Uptr<Node> y){
     if(!x) return y;
     if(!y) return x;
 
@@ -58,49 +73,49 @@ std::unique_ptr<Node<T>> join(std::unique_ptr<Node<T>> x, std::unique_ptr<Node<T
     }
 }
 
-template<typename T>
-std::pair<std::unique_ptr<Node<T>>, std::unique_ptr<Node<T>>> split(std::unique_ptr<Node<T>> x, const T &key){
+template<Comparable T>
+template<typename Compare>
+std::pair<Uptr<typename Treap<T>::Node>, Uptr<typename Treap<T>::Node>> Treap<T>::split(Uptr<Node> x, const T &key){
     if(!x) return {nullptr, nullptr};
-
-    if(key <= x->value){
-        auto [a, b] = split(std::move(x->left), key);
+    
+    if(Compare{}(key,x->value)){
+        auto [a, b] = split<Compare>(std::move(x->left), key);
         x->left = std::move(b);
         return {std::move(a), std::move(x)};
     }
     else{
-        auto [a, b] = split(std::move(x->right), key);
+        auto [a, b] = split<Compare>(std::move(x->right), key);
         x->right = std::move(a);
         return {std::move(x), std::move(b)};
     }
 }
 
-template<typename T>
+template<Comparable T>
 void Treap<T>::insert(const T& value){
-    auto new_node = std::make_unique<Node<T>>(value);
+    if(this->contains(value)) return; //Needs a better solution 
+    auto new_node = std::make_unique<Node>(value);
 
-    auto [a, b] = split(std::move(root), value);
+    auto [a, b] = splitL(std::move(root), value);
     root = join(join(std::move(a), std::move(new_node)), std::move(b));
 }
 
-template<typename T>
+template<Comparable T>
 void Treap<T>::erase(const T& value){
-    auto [a, bc] = split(std::move(root), value);
-    auto [b, c] = split(std::move(bc), value);
+    auto [a, bc] = splitL(std::move(root), value);
+    auto [b, c] = splitU(std::move(bc), value);
 
     root = join(std::move(a), std::move(c));
 }
 
-template<typename T>
-bool Treap<T>::contains(const T& value){
-    Node<T> *cur = root.get();
+template<Comparable T>
+bool Treap<T>::contains(const T& value) const{
+    Node *cur = root.get();
     while(cur){
         if(cur->value == value) return true;
-        else if(cur->value < value) cur = cur->left.get();
+        else if(cur->value > value) cur = cur->left.get();
         else cur = cur->right.get();
     }
 
     return false;
 }
-
-
 #endif
